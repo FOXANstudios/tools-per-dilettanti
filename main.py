f8c7538ca36c4eae7c6a195b73d84a067e93a32b
RedTy_YT <redty2011@gmail.com>
2024-05-03 07:39:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import arpWireless
-import sys
+import subprocess
 
+def execute_command(command):
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print(f"Eseguito comando: {command}")
+    except subprocess.CalledProcessError as e:
+        print(f"Errore durante l'esecuzione del comando '{command}': {e}")
 
 def main():
-    arpWireless.run_batch_file(batch_file_path)
-
+    while True:
+        user_input = input("Inserisci il comando da eseguire (o 'exit' per uscire): ")
+        if user_input.lower() == 'exit':
+            break
+        execute_command(user_input)
 
 if __name__ == "__main__":
     main()
-
-batch_file_path = sys.argv[1]
